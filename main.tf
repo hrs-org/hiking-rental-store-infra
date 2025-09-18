4699073c914f78752ea6397c072941d36f73472d
Feri Setiawan <e1546606@u.nus.edu>
2025-09-18 20:06:38 +0800

    Fix terraform error

--- main.tf
+++ main.tf
@@ -58,7 +58,6 @@ resource "azurerm_mysql_flexible_server" "db" {
   administrator_login = var.mysql_admin_user
   administrator_password = var.mysql_admin_password
   sku_name = "B_Standard_B1ms"
-  storage_mb = 32768
   version = "8.0"
 
   backup_retention_days = 7
