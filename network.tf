fded4bca464ed16db65988b2b837b80703346240
Feri Setiawan <e1546606@u.nus.edu>
2025-09-29 11:56:26 +0800

    Fix prefix

--- network.tf
+++ network.tf
@@ -105,7 +105,7 @@ resource "azurerm_network_security_group" "dmz" {
     source_port_range = "*"
     destination_port_range = "443"
     source_address_prefix = "*"
-    destination_address_prefix = "AzureStorage"
+    destination_address_prefix = "Storage"
   }
   security_rule {
     name = "AllowOutboundKeyVault"
@@ -116,7 +116,7 @@ resource "azurerm_network_security_group" "dmz" {
     source_port_range = "*"
     destination_port_range = "443"
     source_address_prefix = "*"
-    destination_address_prefix = "AzureKeyVault"
+    destination_address_prefix = "KeyVault"
   }
 }
 
